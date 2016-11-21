e532b654897da35ce1dcccb3561c03f60efabe15
Maxie35 <taylor.max1995@gmail.com>
2016-11-21 20:23:34 +0100

    Updated files

--- gulpfile.js
+++ gulpfile.js
@@ -39,23 +39,22 @@ gulp.task('sass', function() {
 });
 
 
-// clean the contents of the distribution directory
+// First clear the js folder before building.
 gulp.task('clean', function () {
-  return del('dist/**/*');
+  return del('js/**/*');
 });
 
-// TypeScript compile & concat
+// TypeScript compile & uglify
 gulp.task('typescript', ['clean'], function () {
   var uglify = $.uglify()
     .on('error', function (e) {
       console.log(e);
   });
-
   return gulp
     .src('app/**/*.ts')
     .pipe($.sourcemaps.init())
     .pipe(typescript(tscConfig.compilerOptions))
-    .pipe($.concat('app.js'))
+    //.pipe($.concat('app.js'))
     .pipe(ngAnnotate({
             // true helps add where @ngInject is not used. It infers.
             // Doesn't work with resolve, so we must be explicit there
@@ -63,14 +62,13 @@ gulp.task('typescript', ['clean'], function () {
         }))
     .pipe(uglify)
     .pipe($.sourcemaps.write())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist/app'))
+    //.pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('js/app'))
     .pipe(browserSync.stream());
 });
 
 
-
-// Static server
+// Static eoserver with browser-sync
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
@@ -83,7 +81,7 @@ gulp.task('browser-sync', function() {
     });
 });
 
-// running "gulp watch"
+// run "gulp watch" just to build.
 gulp.task('watch', ['sass', 'typescript'], (function() {
   gulp.watch(['client/scss/**/*.scss'], ['sass']);
   gulp.watch(['app/**/*.js'], ['typescript'])
@@ -92,7 +90,7 @@ gulp.task('watch', ['sass', 'typescript'], (function() {
 // running "gulp server"
 gulp.task('server', ['sass', 'typescript', 'browser-sync'], (function() {
   gulp.watch(['client/scss/**/*.scss'], ['sass']);
-  gulp.watch(['app/**/*.js'], ['typescript'])
+  gulp.watch(['app/**/*.ts'], ['typescript'])
   gulp.watch("**/*.html").on('change', browserSync.reload);
 }));
 
